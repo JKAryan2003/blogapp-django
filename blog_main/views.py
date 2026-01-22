6b9b54e598f526ca8ce1ec7de2e0c211fc0dc630
JKAryan2003 <jkaryanrout5934@gmail.com>
2026-01-22 20:17:19 +0530

    implement about section

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,16 +1,23 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from blogs.models import Category, Blog
+from blogs.models import Blog, Category
+from assignments.models import About
+# from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
-  categories = Category.objects.all()
-  featured_posts = Blog.objects.filter(is_featured=True, status='Publihsed').order_by('updated_at')
-  posts = Blog.objects.filter(is_featured= False, status='Published')
-
-  context = {
-    'categories': categories,
-    'featured_posts': featured_posts,
-    'posts': posts
-  }
-  return render(request, 'home.html', context)
+    featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
+    posts = Blog.objects.filter(is_featured=False, status='Published')
+    
+    # Fetch about us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+    context = {
+        'featured_posts': featured_posts,
+        'posts': posts,
+        'about': about,
+    }
+    return render(request, 'home.html', context)
