aaa32b47ee4fbf72c959d341837c957897434b41
JKAryan2003 <jkaryanrout5934@gmail.com>
2026-01-22 19:55:12 +0530

    implement posts by category feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -5,10 +5,12 @@ from blogs.models import Category, Blog
 
 def home(request):
   categories = Category.objects.all()
-  featured_posts = Blog.objects.filter(is_featured=True)
+  featured_posts = Blog.objects.filter(is_featured=True, status='Publihsed').order_by('updated_at')
+  posts = Blog.objects.filter(is_featured= False, status='Published')
 
   context = {
     'categories': categories,
-    'featured_posts': featured_posts
+    'featured_posts': featured_posts,
+    'posts': posts
   }
   return render(request, 'home.html', context)
